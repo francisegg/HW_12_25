5e3bef10253b91ad59d253743a241e88319dfd02
tea-egg-tw <bm6hjj2020.tw@gmail.com>
2025-12-25 16:40:34 +0800

    fix: upgrade compileSdk from 34 to 36

--- Lab16_2/app/build.gradle.kts
+++ Lab16_2/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.lab16_2"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.lab16_2"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
